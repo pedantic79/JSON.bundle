09e54df567051f7936aa354af65937b8004eaf63
Dan Nguyen <pedantic@gmail.com>
2019-10-21 18:53:17 -0500

    Support multiple files in same Info.json

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,45 +1,99 @@
-import os, json
+import os
+import json
+
+
+def Start():
+    Log("Starting JSON Agent")
+
+
+def find_info_json(part):
+    directory = os.path.dirname(part)
+    Log("find_info_json " + directory + "]")
+
+    while directory != '':
+        Log("Searching " + directory)
+        path = os.path.join(directory, 'Info.json')
+        if os.path.exists(path):
+            Log("Found Info.json at: " + path)
+            return path
+        else:
+            directory = os.path.dirname(directory)
+
+    raise FileNotFoundError()
+
 
 class JSONAgent(Agent.Movies):
     name = 'JSON Metadata'
-    languages = [Locale.Language.NoLanguage]
+    languages = [Locale.Language.English]
     primary_provider = False
     persist_stored_files = False
     contributes_to = ['com.plexapp.agents.none']
 
     def search(self, results, media, lang):
-        part = media.items[0].parts[0]
-        path = os.path.join(os.path.dirname(part.file), 'Info.json')
+        Log("JSON search: enter")
+        try:
+            part = media.items[0].parts[0]
+            Log("JSON search: [" + part.file + "]")
+            find_info_json(part.file)
+        except Exception as exp:
+            Log("Exception occurred: [" + exp.message + "]")
+            return
 
-        if os.path.exists(path):
-            results.Append(MetadataSearchResult(id = 'null', score = 100))
+        results.Append(MetadataSearchResult(id='null', score=100))
 
     def update(self, metadata, media, lang):
+        Log("JSON update: enter")
         part = media.items[0].parts[0]
-        path = os.path.join(os.path.dirname(part.file), 'Info.json')
 
-        info = json.loads(Core.storage.load(path))
+        try:
+            path = find_info_json(part.file)
+            basename = os.path.basename(part.file)
+            basenameLC = basename.lower()
+        except:
+            return
 
-        try: metadata.title = info['title']
-        except: pass
+        info_full = json.loads(Core.storage.load(path))
+        try:
+            info = info_full[basenameLC]
+            Log("FOUND: [" + basenameLC + "]")
+        except Exception as exp:
+            Log("Exception occurred: [" + exp.message + "]")
+            return
 
-        try: metadata.summary = info['summary']
-        except: pass
+        try:
+            metadata.title = info['title']
+        except:
+            pass
 
-        try: metadata.year = info['year']
-        except: pass
+        try:
+            metadata.summary = info['summary']
+        except:
+            pass
 
-        try: metadata.rating = info['rating']
-        except: pass
+        try:
+            metadata.year = info['year']
+        except:
+            pass
 
-        try: metadata.content_rating = info['content_rating']
-        except: pass
+        try:
+            metadata.rating = info['rating']
+        except:
+            pass
 
-        try: metadata.studio = info['studio']
-        except: pass
+        try:
+            metadata.content_rating = info['content_rating']
+        except:
+            pass
 
-        try: metadata.duration = info['duration']
-        except: pass
+        try:
+            metadata.studio = info['studio']
+        except:
+            pass
+
+        try:
+            metadata.duration = info['duration']
+        except:
+            pass
 
         metadata.directors.clear()
 
@@ -60,14 +114,14 @@ class JSONAgent(Agent.Movies):
         metadata.roles.clear()
 
         try:
-            for r in info['roles']:
+            for r in info['cast']:
                 role = metadata.roles.new()
 
-                try: role.actor = r['actor']
-                except: pass
+                try:
+                    role.name = r
+                except:
+                    pass
 
-                try: role.role = r['role']
-                except: pass
         except:
             pass
 
